04e189b7108dbdf583726d16600dc20934c26cb3
jss有点帅 <1599925910@qq.com>
2024-01-01 15:53:48 +0800

    添加通用样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
 import React from "react";
+import "./styles/index.scss";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <hr />
+      <h1>hello world</h1>
+      <h2>hello world</h2>
+      <h3>hello world</h3>
+      <h4>hello world</h4>
+      <h5>hello world</h5>
+      <h6>hello world</h6>
+      <p>google</p>
+      <ul>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>
+          <ol>
+            <li>2</li>
+            <li>2</li>
+            <li>2</li>
+            <li>2</li>
+            <li>2</li>
+            <li>2</li>
+          </ol>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
